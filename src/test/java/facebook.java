ddaf09be0a866387c1416e93a6d38ba1c92704fd
Santosh Reddy T <Santosh Reddy T>
2016-11-29 19:35:51 -0500

    facebook test

---
+++ src/test/java/facebook.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+
+public class facebook {
+
+	private WebDriver FirefoxDriver;
+	
+	@Test
+	public void san()
+	{
+	WebDriver d =  FirefoxDriver;
+d.get("www.facebook.com");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+}
